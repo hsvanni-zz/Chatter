99d7ea235a2e3b866916e90244ec1a48c39c7867
Nora Vanni <hsvanni@gmail.com>
2015-06-27 15:29:52 -0500

    Added styling

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -2,9 +2,12 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.adapter.jetty :as jetty]
             [hiccup.page :as page]
             [hiccup.form :as form]
-            [ring.middleware.params :refer [wrap-params]]))
+            [ring.util.anti-forgery :as anti-forgery]
+            [environ.core :refer [env]]))
 
 (def chat-messages (atom '()))
 
@@ -17,8 +20,11 @@
   "This generates the HTML for displaying messages"
   [messages]
   (page/html5
-   [:head
-    [:title "chatter"]]
+  [:head
+    [:title "chatter"]
+    (page/include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")
+    (page/include-js  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")
+    (page/include-css "/chatter.css")]
    [:body
     [:h1 "Our Chat App"]
     [:p
@@ -28,15 +34,28 @@
       "Message: " (form/text-field "msg")
       (form/submit-button "Submit"))]
     [:p
-     [:table
+     [:table#messages.table.table-bordered.table-hover
       (map (fn [m] [:tr [:td (:name m)] [:td (:message m)]]) messages)]]]))
 
 (defroutes app-routes
   (GET "/" [] (generate-message-view @chat-messages))
-  (POST "/" {params :params} (generate-message-view
-                               (update-messages! chat-messages
-                                 (get params "name") (get params "msg"))
-                               ))
+  (POST "/" {params :params}
+    (let [name-param (get params "name")
+          msg-param (get params "msg")
+          new-messages (update-messages! chat-messages name-param msg-param)]
+      (generate-message-view new-messages)
+      ))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app (wrap-params app-routes))
+
+(defn init []
+  (println "chatter is starting"))
+
+(defn destroy []
+  (println "chatter is shutting down"))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty #'app {:port port :join? false})))
